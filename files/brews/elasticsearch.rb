d6da5b4bd492e7ff554a9896755264ef19ea9cd2
Michael Phan-Ba <michael@mikepb.com>
2013-03-08 15:29:13 -0800

    ElasticSearch 0.20.5

--- files/brews/elasticsearch.rb
+++ files/brews/elasticsearch.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Elasticsearch < Formula
   homepage 'http://www.elasticsearch.org'
-  url 'http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.2.tar.gz'
-  sha1 '9bedb3638e4fc5a53e264aab3c5ff1a345f22bab'
-  version '0.20.2-boxen1'
+  url 'http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.5.tar.gz'
+  sha1 'e52cd5e7cae2be85d7df65315ec7b91e2e84f83e'
+  version '0.20.5-boxen1'
 
   def cluster_name
     "elasticsearch_#{ENV['USER']}"
